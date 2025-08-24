d5ef22dda837543e054da50affa35bb60fde1c46
Mansi Singhal <manshasinghal143@gmail.com>
2025-08-24 18:05:13 +0530

    Stripe function change

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -7,8 +7,9 @@ import { inngest } from '../inngest/index.js';
 export const getNowPlayingMovies = async (req, res) => {
   try {
     const { data } = await axios.get('https://api.themoviedb.org/3/movie/now_playing', {
-      headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
-      timeout: 10000
+      headers: { 
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.TMDB_API_KEY}` }
     })
     const movies = data.results;
     res.json({ success: true, movies: movies })
